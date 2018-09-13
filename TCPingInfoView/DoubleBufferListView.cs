59d3a859c3b208900f73b4f184f18d54496c2d18
HMBSbige <admin@bige0.com>
2018-09-13 14:11:16 +0800

    Try align vertical grid lines with Headers dividers

--- TCPingInfoView/DoubleBufferListView.cs
+++ TCPingInfoView/DoubleBufferListView.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.Windows.Forms;
 
 namespace TCPingInfoView
 {
@@ -9,6 +10,29 @@ namespace TCPingInfoView
 		{
 			SetStyle(ControlStyles.DoubleBuffer | ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
 			UpdateStyles();
+			OwnerDraw = true;
+		}
+
+		protected override void OnDrawColumnHeader(DrawListViewColumnHeaderEventArgs e)
+		{
+			base.OnDrawColumnHeader(e);
+			e.Graphics.FillRectangle(SystemBrushes.Window, e.Bounds);
+			e.Graphics.DrawRectangle(SystemPens.GradientInactiveCaption, new Rectangle(e.Bounds.X, -1, e.Bounds.Width, e.Bounds.Height));
+			var text = Columns[e.ColumnIndex].Text;
+			const TextFormatFlags cFlag = TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter;
+			TextRenderer.DrawText(e.Graphics, text, Font, e.Bounds, Color.Black, cFlag);
+		}
+
+		protected override void OnDrawItem(DrawListViewItemEventArgs e)
+		{
+			base.OnDrawItem(e);
+			e.DrawDefault = true;
+		}
+
+		protected override void OnDrawSubItem(DrawListViewSubItemEventArgs e)
+		{
+			base.OnDrawSubItem(e);
+			e.DrawDefault = true;
 		}
 	}
 }
