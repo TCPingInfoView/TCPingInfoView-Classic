b65a53d4569e71bec977937a95ad9b2435612b7c
Bruce Wayne <admin@bige0.com>
2019-05-24 11:44:11 +0800

    Init

---
+++ TCPingInfoView-Wpf/App.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using TCPingInfoView.View;
+
+namespace TCPingInfoView
+{
+	internal static class App
+	{
+		[STAThread]
+		private static void Main()
+		{
+			var app = new Application();
+
+			var win = new MainWindow();
+			app.MainWindow = win;
+			win.Show();
+
+			//app.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+			app.Run();
+
+		}
+	}
+}
